dcce7de8ea88d913b219c6fa3bc978ba53148f9a
NovaFox161 <novamaday@gmail.com>
2021-11-28 23:43:48 -0600

    Progress, finally.... I think.

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,22 +1,81 @@
 import styles from "../styles/Home.module.css";
 import * as React from 'react';
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import {Box} from "@material-ui/core";
 
-
-function BrandingRow() {
-    return <div className={"flex flex-col"}>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-    </div>
-}
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        paper: {
+            textAlign: 'center',
+            color: theme.palette.text.secondary,
+        },
+        something: {
+            display: 'flex',
+        },
+        text_stuff: {
+            padding: theme.spacing(2),
+        }
+    }),
+);
 
 export default function Footer() {
-    return <footer className={styles.footer}>
-        <div className="flex flex-row">
-            <BrandingRow/>
-            <BrandingRow/>
-            <BrandingRow/>
-        </div>
-    </footer>
-}
+    const classes = useStyles();
 
+    function BrandingRow() {
+        return (
+            <React.Fragment>
+                <div className={classes.something}>
+                    <image>
+                        <img src="/logos/dark/transparent/logo.png" width={64} alt="DisCal Logo"/>
+                    </image>
+                    <div className={classes.text_stuff}>Text</div>
+                </div>
+            </React.Fragment>
+        );
+    }
+
+    function LinksRow() {
+        return (
+            <React.Fragment>
+                <Grid item xs={4}>
+                    <Box className={classes.paper}>item1</Box>
+                </Grid>
+                <Grid item xs={4}>
+                    <Box className={classes.paper}>item2</Box>
+                </Grid>
+                <Grid item xs={4}>
+                    <Box className={classes.paper}>item3</Box>
+                </Grid>
+            </React.Fragment>
+        );
+    }
+
+    function LegalRow() {
+        return (
+            <React.Fragment>
+                <Grid item xs={4}>
+                    <Box className={classes.paper}>item1</Box>
+                </Grid>
+                <Grid item xs={4}>
+                    <Box className={classes.paper}>item2</Box>
+                </Grid>
+                <Grid item xs={4}>
+                    <Box className={classes.paper}>item3</Box>
+                </Grid>
+            </React.Fragment>
+        );
+    }
+
+    return (
+        <footer className={styles.footer}>
+            <Grid container>
+                <Grid container direction={"column"}>
+                    <BrandingRow/>
+                    <LinksRow/>
+                    <LegalRow/>
+                </Grid>
+            </Grid>
+        </footer>
+    );
+}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,15 @@
-import Document, {Html, Head, Main, NextScript} from 'next/document'
+import Document, {Head, Html, Main, NextScript} from 'next/document'
 
 class MyDocument extends Document {
     render() {
         return (
             <Html>
                 <Head>
-                    <link rel="icon" href="/favicon.ico" />
+                    <link rel="icon" href="/favicon.ico"/>
+
+                    <link rel="stylesheet"
+                          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
                 </Head>
                 <body>
                 <Main/>
