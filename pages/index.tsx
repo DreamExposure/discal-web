09d53dbc44b0ffa5eb5f766c8663081830295599
NovaFox161 <novamaday@gmail.com>
2021-12-28 20:45:36 -0500

    Soon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
+import Container from "../components/container";
 
 const Home: NextPage = () => {
   return (
-    <>
+    <Container>
+        <h1 className='text-7xl font-semibold text-discal-blue tracking-wide text-center my-20'>
+            Soon...
+        </h1>
 
-    </>
+    </Container>
   )
 }
 
