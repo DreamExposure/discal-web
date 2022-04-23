1cd4d4819f58146b78591c58c3de286b926105b5
NovaFox161 <novamaday@gmail.com>
2022-04-23 15:24:33 -0500

    Fix container sizing

--- components/container.tsx
+++ components/container.tsx
@@ -1,9 +1,7 @@
-import type { Props } from "../lib/types";
+import type {Props} from "../lib/types";
 
 export default function Container(props: Props) {
-    return <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="max-w-5xl mx-auto pt-10">
-            {props.children}
-        </div>
+    return <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-10">
+        {props.children}
     </div>
 }
